fa745682ada62b07b2c702afcf321976dbff313b
Rahul967 <contactrahulkrsingh@gmail.com>
2023-06-14 17:58:47 +0530

    Added test folder structure to app builder

---
+++ service/routes/appbuilder.routes.js
@@ -0,0 +1,23 @@
+// const express = require("express");
+
+// const testController = require("../controllers/test.controller");
+
+// const testMiddleware = require("../middlewares/test.middleware");
+
+// const testRouter = express.Router();
+
+// testRouter.get("/", testMiddleware, testController.testGetFunc);
+// testRouter.post("/", testMiddleware, testController.testPostFunc);
+// testRouter.put("/", testMiddleware, testController.testPutFunc);
+// testRouter.patch("/", testMiddleware, testController.testPatchFunc);
+
+// module.exports = testRouter;
+
+
+// -------------------------------------------------------
+
+// these apis were not found
+
+// // export const urls = {
+//   STATUS_CHANGE_FORM: "status/:model/:id/:status",
+// // }; 
