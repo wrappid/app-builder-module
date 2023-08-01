d3e4248c0b6929e2a4714d46a9af0ae951b89626
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-08-01 12:54:18 +0530

    #6 few changes made

--- app/components/StatusChangeCommentHistory.js
+++ app/components/StatusChangeCommentHistory.js
@@ -5,42 +5,49 @@ export default function StatusChangeCommentHistory(props) {
   const { comments } = data;
 
   return (
-    <CoreStack direction="column">
-      {comments?.map((comment, index) => (
-        <CoreGrid key={index}>
+    <>
+      {comments && <CoreStack direction="column">
+        <CoreGrid>
           <CoreBox gridProps={{ gridSize: 2 }}>
             <CoreLabel>Current Status:</CoreLabel>
-
-            <br />
-
-            <CoreLabel>{comment?.currentStatus}</CoreLabel>
           </CoreBox>
-
+          
           <CoreBox gridProps={{ gridSize: 2 }}>
             <CoreLabel>Next Status:</CoreLabel>
-
-            <br />
-
-            <CoreLabel>{comment?.nextStatus}</CoreLabel>
           </CoreBox>
 
           <CoreBox gridProps={{ gridSize: 6 }}>
             <CoreLabel>Comment:</CoreLabel>
-
-            <br />
-
-            <CoreLabel>{comment?.comment}</CoreLabel>
           </CoreBox>
 
           <CoreBox gridProps={{ gridSize: 2 }}>
             <CoreLabel>User Id:</CoreLabel>
-
-            <br />
-
-            <CoreLabel>{comment?.userId}</CoreLabel>
           </CoreBox>
         </CoreGrid>
-      ))}
-    </CoreStack>
+      </CoreStack>
+      }
+      
+      <CoreStack direction="column">
+        {comments?.map((comment, index) => (
+          <CoreGrid key={index}>
+            <CoreBox gridProps={{ gridSize: 2 }}>
+              <CoreLabel>{comment?.currentStatus}</CoreLabel>
+            </CoreBox>
+
+            <CoreBox gridProps={{ gridSize: 2 }}>
+              <CoreLabel>{comment?.nextStatus}</CoreLabel>
+            </CoreBox>
+
+            <CoreBox gridProps={{ gridSize: 6 }}>
+              <CoreLabel>{comment?.comment}</CoreLabel>
+            </CoreBox>
+
+            <CoreBox gridProps={{ gridSize: 2 }}>
+              <CoreLabel>{comment?.userId}</CoreLabel>
+            </CoreBox>
+          </CoreGrid>
+        ))}
+      </CoreStack>
+    </>
   );
 }
