bb5d9a5685bf06a13347466ab4ee93ed7a03421e
Aditya Jaiswal <aditya.jaiswal.cse23@heritageit.edu.in>
2023-08-04 17:54:33 +0530

    + moved contents from core Ref issue 47 of core

---
+++ app/formValidationMap.registry.js
@@ -0,0 +1,6 @@
+import * as yup from "yup";
+
+export const ValidationRegistry = {
+  pageFormSchema : { appComponent: yup.string().required("App component is required") },
+  routeFormSchema: { pageRef: yup.string().required("Page refference is required") }
+};
---
+++ app/urls.registry.js
@@ -0,0 +1,6 @@
+export const urls = {
+  BUSINESS_ENTITY: "business_entity",
+  FORMS          : "forms",
+  ROUTES         : "routes",
+  TABLES         : "tables",
+};
