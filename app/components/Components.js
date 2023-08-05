d0c3c54da7edff9d3e788799c233af71e0963a83
Saif Alam Khan <54716802+saifalamkhan373@users.noreply.github.com>
2023-08-05 13:22:33 +0530

    enhancement #9

--- app/components/Components.js
+++ app/components/Components.js
@@ -1,28 +1,64 @@
-import { CoreH5, CoreComponent, /* CoreMarkdownViewer, */ CoreComponentsRegistryDocs, CoreTypographyBody1, CoreStack } from "@wrappid/core";
+import {
+  CoreH3,
+  CoreH4,
+  CoreBox,
+  CoreComponentsRegistryDocs,
+  CoreStack,
+  CoreTypographyBody1,
+  CoreDivider,
+  CoreAvatar,
+  CoreClasses,
+  CoreComponent,
+} from "@wrappid/core";
 
 export default function Components() {
   return (
-    <>
-      {Object.keys(CoreComponentsRegistryDocs || {}).map(componentName => {
+    <CoreBox>
+      {Object.keys(CoreComponentsRegistryDocs || {}).map((componentName) => {
+        const componentData =
+          CoreComponentsRegistryDocs[componentName]?.documentation;
         return (
-          <>
-            <CoreH5>{componentName}</CoreH5>
-
-            <CoreStack direction="row">
-              <CoreComponent componentName={componentName} />
-
-              <CoreComponent componentName={componentName} {...CoreComponentsRegistryDocs[componentName].categoryGroup[0].propsGroups[0]} />
-            </CoreStack>
-
-            {/* <CoreTypographyBody1 code={true}>
-              {JSON.stringify(CoreComponentsRegistryDocs[componentName], null, 2)}
-            </CoreTypographyBody1> */}
-
-            {/* <CoreMarkdownViewer url={CoreComponentsRegistry[componentName].readme} /> */}
-          </>
+          <CoreBox key={componentName}>
+            <CoreH3>{componentName}</CoreH3>
+            {componentData.categoryGroup.map((category, index) => (
+              <CoreBox key={index}>
+                <CoreH4>{category.title}</CoreH4>
+                <CoreTypographyBody1>
+                  {category.description}
+                </CoreTypographyBody1>
+                <CoreStack
+                  direction="row"
+                  borderRadius="10px"
+                  styleClasses={[
+                    CoreClasses.BORDER.BORDER,
+                    CoreClasses.BORDER.BORDER_PRIMARY_LIGHT,
+                    CoreClasses.PADDING.P3,
+                    CoreClasses.ALIGNMENT.JUSTIFY_CONTENT_CENTER,
+                  ]}
+                >
+                  {category.propsGroups.map((propsGroup, innerIndex) => {
+                    return <CoreComponent key={innerIndex} componentName={componentName} {...propsGroup} />
+                  })}                 
+                </CoreStack>
+                <CoreStack
+                  borderRadius="10px"
+                  styleClasses={[
+                    CoreClasses.BORDER.BORDER,
+                    CoreClasses.BORDER.BORDER_PRIMARY_LIGHT,
+                    CoreClasses.PADDING.P3,
+                  ]}
+                >
+                  <CoreTypographyBody1 code={true}>
+                  {category.propsGroups.map(( ) => {
+                    return  `<${componentName } />`;
+                  })}    
+                  </CoreTypographyBody1>
+                </CoreStack>
+              </CoreBox>
+            ))}
+          </CoreBox>
         );
       })}
-
-    </>
+    </CoreBox>
   );
 }
