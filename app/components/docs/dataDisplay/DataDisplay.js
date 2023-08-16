4a7822d3e4e05be3716386f8f68f72fd599ab577
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-08-16 14:42:00 +0530

    + basic struct ready

--- app/components/docs/dataDisplay/DataDisplay.js
+++ app/components/docs/dataDisplay/DataDisplay.js
@@ -1,13 +1,15 @@
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars, unused-imports/no-unused-imports
 import React from "react";
 
 import { CoreH5 } from "@wrappid/core";
+
 import Heading from "./Heading";
 
 export default function DataDisplay() {
   return (
     <>
       <CoreH5>Data Display Components</CoreH5>
+
       <Heading />
     </>
   );
