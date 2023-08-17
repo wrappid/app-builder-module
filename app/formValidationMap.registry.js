6bfc5a61594c822fd46e0ad8e8a008e1e6908373
Ananta <antaghosh@gmail.com>
2023-08-17 16:01:36 +0530

    fix export

--- app/formValidationMap.registry.js
+++ app/formValidationMap.registry.js
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-export const ValidationRegistry = {
+export const ValidationsRegistry = {
   pageFormSchema : { appComponent: yup.string().required("App component is required") },
   routeFormSchema: { pageRef: yup.string().required("Page refference is required") }
 };
