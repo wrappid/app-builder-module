9ddf0e57741cec2c78cea5c5aefe3b89344d6683
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-08-26 16:08:15 +0530

    + /business/tables/:database done

--- service/controllers/database.controller.js
+++ service/controllers/database.controller.js
@@ -1,19 +1,27 @@
+const { databaseProvider } = require("@wrappid/service-core");
+
 module.exports.getDatabaseTables = (req, res) => {
     try {
         let database = req.params.database;
         let requestedDBTables = databaseProvider[database].models;
+        let searchValue = req.query._searchValue;
 
-        let _data = {
-            rows: Object.keys(requestedDBTables)
+        let searchedTables = Object.keys(requestedDBTables);
+
+        if (searchValue) {
+            searchedTables = Object.keys(requestedDBTables)
                 ?.filter((key) => {
                     return key
                         .toLocaleLowerCase()
-                        .includes(req.query._searchValue?.toLocaleLowerCase());
-                })
-                .map((key) => {
-                    return { id: key, name: getNormalCaseFromCamelCase(key) };
+                        .includes(searchValue?.toLocaleLowerCase());
+                });
+        }
+
+        let _data = {
+            rows: searchedTables.map((key) => {
+                    return { id: key, name: key };
                 }),
-            totalRecords: Object.keys(requestedDBTables).length,
+            totalRecords: Object.keys(searchedTables).length,
         };
 
         res.status(200).json({
