412ed3aa67b2c0c81ad7bda97863c065b9effa6a
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-08-26 16:15:33 +0530

    + route check in

--- service/routes/appbuilder.routes.js
+++ service/routes/appbuilder.routes.js
@@ -4,26 +4,20 @@ const appBuilderController = require("../controllers/appBuilder.controller");
 
 const appBuilderRouter = express.Router();
 
+//database
 appBuilderRouter.get("/business/tables/:database", appBuilderController.getTables);
 appBuilderRouter.get("/business/tables/attributes/:database/:table", appBuilderController.getAttributes);
+appBuilderRouter.get("/noauth/business/columns/:entity", appBuilderController.getBusinessEntityColumns);
+
+//business controller
 appBuilderRouter.get("/business/entities", appBuilderController.getBusinessEntities);
 appBuilderRouter.get("/business/count/:entity", appBuilderController.getEntityData);
-
-appBuilderRouter.get("/noauth/business/columns/:entity", appBuilderController.getBusinessEntityColumns);
 appBuilderRouter.get("/business/individual/:entity", appBuilderController.getIndividualEntityData);
 appBuilderRouter.get("/business/all/:entity", appBuilderController.getAllEntityData);
 appBuilderRouter.get("/noauth/business/all/:entity", appBuilderController.noAuthGetAllEntityData);
 
+// forms controller
 appBuilderRouter.get("/noauth/formSchema/:formID", appBuilderController.getNoAuthFormSchema);
-
 appBuilderRouter.get("/formSchema/:formID", appBuilderController.getFormSchemaWithFormId);
 
 module.exports = appBuilderRouter;
-
-// -------------------------------------------------------
-
-// these apis were not found
-
-// // export const urls = {
-//   STATUS_CHANGE_FORM: "status/:model/:id/:status",
-// // }; 
